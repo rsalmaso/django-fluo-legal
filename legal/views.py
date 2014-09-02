0741bf4acc7d7ad39a95c690abe2e4f35e917f2d
Raffaele Salmaso <raffaele@salmaso.org>
2014-09-02 21:22:52 +0200

    added a default view to show current/versioned TermsOfService

---
+++ legal/views.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2007-2014, Raffaele Salmaso <raffaele@salmaso.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, Http404
+from fluo.views import View
+from .models import TermsOfService
+
+
+class IndexView(View):
+    def get(self, request):
+        try:
+            tos = TermsOfService.objects.current
+        except TermsOfService.DoesNotExist:
+            raise Http404
+        return HttpResponseRedirect(reverse('legal-version', args=[tos.version]))
+
+
+class TermsOfServiceView(View):
+    def get(self, request, version):
+        tos = get_object_or_404(TermsOfService, version=version)
+        return render(request, 'legal/terms.html', {
+            'tos': tos,
+        })
